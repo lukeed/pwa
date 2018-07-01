329f4dbd48a0b24e3446c10e0399c142806a63dd
Luke Edwards <luke.edwards05@gmail.com>
2018-06-30 17:39:58 -0700

    add config custom-chain hierarchy

--- packages/cli/lib/utils.js
+++ packages/cli/lib/utils.js
@@ -3,3 +3,7 @@ const { existsSync, statSync } = require('fs');
 exports.isDir = function (str) {
 	return existsSync(str) && statSync(str).isDirectory();
 }
+
+exports.toFile = function (str) {
+	return existsSync(str) && require(str);
+}
--- packages/cli/lib/webpack/index.js
+++ packages/cli/lib/webpack/index.js
@@ -1,15 +1,17 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
-const { isDir } = require('../utils');
+const rr = require('require-relative');
 const toConfig = require('./config');
+const $ = require('../utils');
 
 module.exports = function (src, opts) {
 	opts.cwd = resolve(opts.cwd || '.');
 	opts.production = !!opts.production;
+	delete opts._; // useless
 
 	// use root if "/src" is missing
 	src = resolve(opts.cwd, src || 'src');
-	src = isDir(src) ? src : opts.cwd;
+	src = $.isDir(src) ? src : opts.cwd;
 
 	let config = toConfig(src, opts);
 
@@ -20,6 +22,24 @@ module.exports = function (src, opts) {
 
 	if (opts.export) {
 		// config.push Prerender anywhere
+	}
+
+	// Apply "@pwa/preset"s if any
+	let pkg = resolve(opts.cwd, 'package.json');
+	if (pkg = $.toFile(pkg)) {
+		let devs = Object.keys(pkg.devDependencies || {});
+		devs.filter(x => x.indexOf('@pwa/preset') == 0).forEach(str => {
+			console.log('[PWA] Applying preset :: `%s`', str);
+			str = rr.resolve(str, opts.cwd);
+			require(str)(config, opts); // allow throw
+		});
+	}
+
+	// Apply Custom Config mutations
+	let custom = resolve(opts.cwd, 'pwa.config.js');
+	if (custom = $.toFile(custom)) {
+		console.log('[PWA] Loading custom config');
+		custom(config, opts);
 	}
 
 	return webpack(config);
