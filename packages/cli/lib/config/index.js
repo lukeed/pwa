4a1e324274b8985fc8d1c71945593d0b67d4509d
Luke Edwards <luke.edwards05@gmail.com>
2018-07-02 10:25:05 -0700

    add baseline babel config

--- packages/cli/lib/config/index.js
+++ packages/cli/lib/config/index.js
@@ -1,5 +1,22 @@
 exports.babel = {
-	// TODO
+	babelrc: false,
+	presets: [
+		['env', {
+			loose: true,
+			modules: false,
+			targets: {
+				uglify: true,
+				// "browsers" are injected
+			},
+			exclude: [
+				'transform-regenerator',
+				'transform-es2015-typeof-symbol'
+			]
+		}]
+	],
+	plugins: [
+		require.resolve('babel-plugin-syntax-dynamic-import')
+	]
 }
 
 // @see https://jamie.build/last-2-versions
@@ -7,16 +24,16 @@ exports.browsers = [
 	'>0.25%',
 	'not ie 11',
 	'not op_mini all'
-];
+]
 
 // Any PostCSS config
 // ~> "autoprefixer" will be replaced
 exports.postcss = {
 	plugins: { autoprefixer:{} }
-};
+}
 
 // Empty ~> no customization
-exports.uglify = {};
+exports.uglify = {}
 
 // Leave this commented out -- is visual docs
 // exports.webpack = function (config, opts) {}
