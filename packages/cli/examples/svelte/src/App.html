629a979c5a8491fffa209a5e948f5d4ad543ce68
Luke Edwards <luke.edwards05@gmail.com>
2018-07-04 21:48:31 -0700

    add Svelte example (temporary)

---
+++ packages/cli/examples/svelte/src/App.html
@@ -0,0 +1,7 @@
+<h1>Hello {name}!</h1>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
---
+++ packages/cli/examples/svelte/src/index.js
@@ -0,0 +1,9 @@
+import App from './App.html';
+import './index.css';
+
+window.app = new App({
+	target: document.body,
+	data: {
+		name: 'world'
+	}
+});
