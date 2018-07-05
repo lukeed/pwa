6bc1b558e83040ff154899013cf55962f5c956f7
Luke Edwards <luke.edwards05@gmail.com>
2018-07-04 21:47:40 -0700

    preset: add Svelte

---
+++ packages/preset-svelte/index.js
@@ -0,0 +1,46 @@
+const { join } = require('path');
+
+exports.webpack = function (config, opts) {
+	let isProd = opts.production;
+
+	if (config.resolve.extensions) {
+		config.resolve.extensions.push('.html', '.svelte');
+	} else {
+		config.resolve.extensions = ['.wasm', '.mjs', '.js', '.json', '.html', '.svelte'];
+	}
+
+	if (config.resolve.mainFields) {
+		config.resolve.mainFields.push('svelte');
+	} else {
+		config.resolve.mainFields = ['svelte', 'browser', 'module', 'main'];
+	}
+
+	// turn off "raw-loader" on *.html files
+	config.module.rules.forEach(obj => {
+		if (obj.test.test('index.html')) {
+			let rgx, arr=obj.test.toString().match(/\((.*)\)/);
+			if (arr && arr[1]) {
+				rgx = arr[1].split('|').filter(x => x !== 'html').join('|');
+				obj.test = new RegExp('\\.(' + rgx + ')$');
+			}
+		}
+	});
+
+	config.module.rules.push({
+		test: /\.(html|svelte)$/,
+		exclude: [
+			// Ignore root template (eg src/index.html)
+			join(config.context, 'index.html')
+		],
+		use: {
+			loader: 'svelte-loader',
+			options: {
+				emitCss: true,
+				hotReload: true,
+				hydratable: true,
+				skipIntroByDefault: true,
+				nestedTransitions: true,
+			}
+		}
+	});
+}
