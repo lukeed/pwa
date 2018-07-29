be35fbaafa896d38156dae4c099de0caa7ad369c
Luke Edwards <luke.edwards05@gmail.com>
2018-07-29 16:19:03 -0700

    add `logger` util;
    - wraps package names w/ addl formatting

--- packages/cli/log.js
+++ packages/cli/log.js
@@ -2,6 +2,7 @@ const $ = require('kleur');
 
 const PWA = $.bold('[PWA]');
 const SPACER = ' '.repeat(6); // "[PWA] "
+const RGX = /(@pwa\/(.*))($|\s)/g; //~> @pwa/foo-bar
 
 function print(color, msg) {
 	console.log($[color](PWA), msg.includes('\n') ? msg.replace(/(\r?\n)/g, '$1' + SPACER) : msg);
@@ -12,3 +13,7 @@ exports.warn = print.bind(null, 'yellow');
 exports.success = print.bind(null, 'green');
 exports.info = print.bind(null, 'cyan');
 exports.error = print.bind(null, 'red');
+
+exports.logger = msg => {
+	exports.info(msg.includes('@pwa') ? msg.replace(RGX, $.magenta.underline('$1') : msg);
+};
