7bd662472b2b068c06b693af109bd0a61d4bab6c
Luke Edwards <luke.edwards05@gmail.com>
2018-07-29 23:29:01 -0700

    stub default `template` file

--- packages/core/webpack/html.js
+++ packages/core/webpack/html.js
@@ -1,10 +1,11 @@
+const { join } = require('path');
 const { readdirSync } = require('fs');
 
 module.exports = function (src, opts) {
 	let config = {};
 	let rgx = /index\.(html|hbs|ejs)$/;
 	let template = opts.template || readdirSync(src).find(x => rgx.test(x));
-	template !== void 0 && (config.template = template);
+	config.template = template || join(__dirname, 'template.html');
 
 	config.minify = opts.production && {
 		removeComments: true,
