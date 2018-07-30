c9125995886ae7962da23878332395547e2047b5
Luke Edwards <luke.edwards05@gmail.com>
2018-07-29 22:48:30 -0700

    use `kB` as minimum size unit

--- packages/cli/lib/util/pretty.js
+++ packages/cli/lib/util/pretty.js
@@ -1,13 +1,12 @@
-const UNITS = ['B ', 'kB', 'MB', 'GB'];
+const UNITS = ['B', 'kB', 'MB', 'GB'];
 
 exports.size = function (val) {
-	let exp = Math.min(Math.floor(Math.log10(val) / 3), UNITS.length - 1);
+	let exp = Math.min(Math.floor(Math.log10(val) / 3), UNITS.length - 1) || 1;
 	let out = (val / Math.pow(1e3, exp)).toPrecision(3);
 	let idx = out.indexOf('.');
-	// ensure non-byte unit has 2 decimal
-	if (exp > 0 && idx === -1) {
+	if (idx === -1) {
 		out += '.00';
-	} else if (exp > 0 && (out.length - idx - 1 !== 2)) {
+	} else if (out.length - idx - 1 !== 2) {
 		out = (out + '00').substring(0, idx + 3); // 2 + 1 for 0-based
 	}
 	return out + ' ' + UNITS[exp];
