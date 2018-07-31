15726b51f754d794bf3cc3597cb49c715962e60b
Luke Edwards <luke.edwards05@gmail.com>
2018-07-31 10:13:51 -0700

    [BIG] use Chrome DevTools to scrape page(s);
    - writes files to output
    - iterates thru routes sequentially
    - cleans up & shuts down (happy path)

--- packages/cli/lib/build.js
+++ packages/cli/lib/build.js
@@ -1,7 +1,9 @@
 const colors = require('kleur');
+const { join } = require('path');
 const { gzipSync } = require('zlib');
 const pretty = require('./util/pretty');
 const log = require('./util/log');
+const fs = require('./util/fs');
 
 const _ = ' ';
 const gutter = _.repeat(4);
@@ -9,6 +11,32 @@ const levels = ['cyan', 'yellow', 'red']; // sizes
 const th = str => colors.dim.bold.italic.underline(str);
 const lpad = (str, max) => _.repeat(max - str.length) + str;
 const rpad = (str, max) => str + _.repeat(max - str.length);
+
+function chain(arr) {
+	return new Promise((res, rej) => {
+		let out=[];
+		let run = fn => (fn.then ? Promise.resolve(fn) : fn()).then(x => out.push(x));
+		let next = () => (arr.length > 0) ? run(arr.shift()).then(next) : res(out);
+		next();
+	});
+}
+
+const expression = 'document.documentElement.outerHTML';
+
+function dump(chrome, dest, base, pathname) {
+	let url = base + pathname;
+	let { Page, Runtime } = chrome;
+	let file = join(pathname, 'index.html');
+
+	return Page.navigate({ url }).then(() => {
+		return Page.loadEventFired().then(() => {
+			return Runtime.evaluate({ expression }).then(r => {
+				log.info(`Wrote file: ${colors.bold.magenta(file)}`);
+				return fs.writer(join(dest, file)).end(r.result.value);
+			});
+		});
+	});
+}
 
 module.exports = function (src, opts) {
 	opts.production = true;
@@ -59,6 +87,7 @@ module.exports = function (src, opts) {
 			const glob = require('tiny-glob/sync');
 			const { createServer } = require('http');
 			const { launch } = require('chrome-launcher');
+			const remote = require('chrome-remote-interface');
 
 			// Get routes from file structure
 			let src = ctx.options.resolve.alias['@pages'];
@@ -71,11 +100,28 @@ module.exports = function (src, opts) {
 			let onNoMatch = res => fn({ path:'/' }, res, r => (r.statusCode=404,r.end()));
 			let server = createServer(fn=sirv(dest, { onNoMatch })).listen();
 
-			// let chromeFlags = ['--headless', '--disable-gpu'];
-			let startingUrl = 'http://localhost:' + server.address().port;
-			log.log(`Started local server on ${ colors.white.bold.underline(startingUrl) }`);
+			let chromeFlags = ['--headless', '--disable-gpu'];
+			let base = 'http://localhost:' + server.address().port;
+			log.log(`Started local server on ${ colors.white.bold.underline(base) }`);
 
-			launch({ startingUrl }).then(proc => {
+			launch({ chromeFlags }).then(proc => {
+				return remote({ port:proc.port }).then(chrome => {
+					let { Page, Network, DOM } = chrome;
+					let scrape = dump.bind(null, chrome, dest, base);
+					return Promise.all([Page, Network, DOM].map(x => x.enable())).then(() => {
+						return chain(routes.map(x => () => scrape(x))).then(arr => {
+							proc.kill();
+							chrome.close();
+							server.close();
+							log.log('Shutdown local server\n');
+							let sfx = arr.length > 1 ? 's' : '';
+							let num = colors.italic.bold.green(arr.length);
+							log.success(`Export complete!\nGenerated ${num} page${sfx} 🙌🏼`);
+						}).catch(err => {
+							console.log('> error', err); //TODO
+						});
+					});
+				});
 			});
 		}
 	});
