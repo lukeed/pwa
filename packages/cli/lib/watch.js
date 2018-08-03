5b112d3fe1b321982b7512eed75cbefc04a78287
Luke Edwards <luke.edwards05@gmail.com>
2018-08-02 23:02:29 -0700

    add `watch` output w/ rough formatting

--- packages/cli/lib/watch.js
+++ packages/cli/lib/watch.js
@@ -1,5 +1,6 @@
-const { join } = require('path');
 const colors = require('kleur');
+const { join, relative } = require('path');
+const pretty = require('./util/pretty');
 const log = require('./util/log');
 
 const { HOST, PORT } = process.env;
@@ -16,6 +17,35 @@ module.exports = function (src, opts) {
 
 	let port = PORT || opts.port || 8080;
 	let hostname = HOST || opts.host || 'localhost';
+	let protocol = opts.https ? 'https' : 'http';
+
+	if (!opts.quiet) {
+		let uri = require('url').format({ protocol, hostname, port });
+		log.log(`Starting development server on ${ colors.white.bold.underline(uri) }`);
+
+		let format = require('webpack-format-messages');
+		c.hooks.invalid.tap('PWA', file => {
+			file = relative(cwd, file);
+			log.info(`File changed: ${ colors.white.bold(file) }`);
+		});
+		c.hooks.done.tap('PWA', stats => {
+			let out, sfx, { errors, warnings }=format(stats);
+			if (errors.length > 0) {
+				sfx = errors.length > 1 ? 's' : '';
+				out = `Failed to compile! Found ${ colors.red.bold(errors.length) } error${sfx}:`;
+				errors.forEach(x => (out += '\n' + x));
+				return log.error(out);
+			}
+			if (warnings.length > 0) {
+				sfx = warnings.length > 1 ? 's' : '';
+				out = `Compiled with ${ colors.yellow.bold(warnings.length) } warning${sfx}:`;
+				warnings.forEach(x => (out += '\n' + x));
+				log.warn(out);
+			}
+			log.success(`Rebuilt in ${pretty.time(stats.endTime - stats.startTime)}`);
+		});
+	}
+
 	let server = new Server(c, {
 		port,
 		hot: true,
