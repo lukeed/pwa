0ff9ee05364ff4527aeb263f4a84a713d1b4589b
Luke Edwards <luke.edwards05@gmail.com>
2018-08-04 16:34:19 -0700

    ~> improve spacing

--- packages/cli/lib/watch.js
+++ packages/cli/lib/watch.js
@@ -46,28 +46,32 @@ module.exports = function (src, opts) {
 	}
 
 	if (!opts.quiet) {
+		let format = require('webpack-format-messages');
 		let uri = require('url').format({ protocol, hostname, port });
 		log.log(`Starting development server on ${ colors.white.bold.underline(uri) }`);
 
-		let format = require('webpack-format-messages');
 		c.hooks.invalid.tap('PWA', file => {
 			file = relative(cwd, file);
 			log.info(`File changed: ${ colors.white.bold(file) }`);
 		});
+
 		c.hooks.done.tap('PWA', stats => {
-			let out, sfx, { errors, warnings }=format(stats);
+			let { errors, warnings } = format(stats);
+
 			if (errors.length > 0) {
-				sfx = errors.length > 1 ? 's' : '';
-				out = `Failed to compile! Found ${ colors.red.bold(errors.length) } error${sfx}:`;
+				let sfx = errors.length > 1 ? 's' : '';
+				let out = `Failed to compile! Found ${ colors.red.bold(errors.length) } error${sfx}:`;
 				errors.forEach(x => (out += '\n' + x));
 				return log.error(out);
 			}
+
 			if (warnings.length > 0) {
-				sfx = warnings.length > 1 ? 's' : '';
-				out = `Compiled with ${ colors.yellow.bold(warnings.length) } warning${sfx}:`;
+				let sfx = warnings.length > 1 ? 's' : '';
+				let out = `Compiled with ${ colors.yellow.bold(warnings.length) } warning${sfx}:`;
 				warnings.forEach(x => (out += '\n' + x));
 				log.warn(out);
 			}
+
 			log.success(`Rebuilt in ${pretty.time(stats.endTime - stats.startTime)}`);
 		});
 	}
