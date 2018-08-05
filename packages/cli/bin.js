8ee915b22e8722df7e21ea07fc2614c6407279ec
Luke Edwards <luke.edwards05@gmail.com>
2018-08-04 21:38:20 -0700

    cli: use `-o` alias for `--dest` flag

--- packages/cli/bin.js
+++ packages/cli/bin.js
@@ -16,12 +16,12 @@ sade('pwa')
 	.command('build [src]')
 	.describe('Build production assets')
 	.option('--analyze', 'Launch interactive Analyzer to inspect production bundle(s)')
-	.option('--dest', 'Path to output directory', 'build')
+	.option('-o, --dest', 'Path to output directory', 'build')
 	.action(build)
 
 	.command('export [src]')
 	.describe('Export pre-rendered pages')
-	.option('--dest', 'Path to output directory', 'build')
+	.option('-o, --dest', 'Path to output directory', 'build')
 	.action((src, opts) => {
 		opts.export = true;
 		build(src, opts);
