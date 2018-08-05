8192dad1d4fa27a7092add01488dedba009bea48
Luke Edwards <luke.edwards05@gmail.com>
2018-08-04 23:59:27 -0700

    preact: cleanup config

--- examples/preact/pwa.config.js
+++ examples/preact/pwa.config.js
@@ -1,17 +1,3 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 exports.browsers = [
 	'last 2 Chrome version'
 ]
-
-// TEMP
-exports.webpack = function (config, env) {
-	if (env.production) {
-		config.plugins.push(
-			new CopyWebpackPlugin([{
-				from: 'assets',
-				to: 'assets'
-			}])
-		);
-	}
-}
