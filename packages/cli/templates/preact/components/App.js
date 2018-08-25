47f1d0d3a73b319b208211bb5665c66efbc78c30
Luke Edwards <luke.edwards05@gmail.com>
2018-08-24 21:25:53 -0700

    templates: begin preact & svelte

---
+++ packages/cli/templates/preact/components/App.js
@@ -0,0 +1,9 @@
+import style from './index';
+
+export default function (props) {
+	return (
+		<div class={style.app}>
+			<h1>Hello</h1>
+		</div>
+	);
+}
---
+++ packages/cli/templates/svelte/index.js
@@ -0,0 +1,6 @@
+import App from '@components/App.html';
+import './index.{{style}}';
+
+window.app = new App({
+	target: document.querySelector('#app')
+});
