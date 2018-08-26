962943a49c6f934704382fdef4134bfe6561b099
Luke Edwards <luke.edwards05@gmail.com>
2018-08-26 16:46:40 -0700

    add `webpack-assets` package

---
+++ packages/webpack-assets/index.js
@@ -0,0 +1,51 @@
+const { relative } = require('path');
+const { readFileSync } = require('fs');
+const glob = require('tiny-glob/sync');
+
+const NAME = '@pwa/webpack-assets';
+
+class CopyAssets {
+	constructor(src, dir) {
+		this.src = src;
+		this.dir = dir;
+	}
+
+	apply(compiler) {
+		let src = this.src || compiler.options.context; // "src" | root
+		let dir = this.dir || compiler.options.resolve.alias['@assets'];
+
+		let arr = glob('**/*.*', { cwd:dir, absolute:true });
+		let set = new Set(arr);
+
+		// Remove a file from the set if within `dir`.
+		// ~> the "filename" may be different
+		// ~~> eg; via `vue-loader` (already transformed)
+		function toPurge(mod, filename) {
+			let m = mod.resource;
+			if (m && m.startsWith(dir)) {
+				if (filename) set.delete(filename);
+				set.delete(m);
+			}
+		}
+
+		compiler.hooks.compilation.tap(NAME, c => {
+			c.hooks.moduleAsset.tap(NAME, toPurge);
+			c.hooks.afterOptimizeModules.tap(NAME, mods => {
+				mods.forEach(toPurge);
+			});
+		});
+
+		// Add any remaining "/assets/**" to Webpack tree
+		compiler.hooks.shouldEmit.tap(NAME, c => {
+			set.forEach(file => {
+				let val = readFileSync(file);
+				c.assets[relative(src, file)] = {
+					size: () => val.length,
+					source: () => val
+				};
+			});
+		});
+	}
+}
+
+module.exports = CopyAssets;
