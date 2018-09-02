fce0aebb86f67e1c9d4996385d5b352cd10a6b2b
Luke Edwards <luke.edwards05@gmail.com>
2018-09-02 16:04:44 -0700

    template: add GA for react

--- packages/cli/templates/react/index.js
+++ packages/cli/templates/react/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GAnalytics from 'ganalytics';
 import App from '@components/App';
 import './index.{{style}}';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
 if (process.env.NODE_ENV === 'production') {
+	window.ga = new GAnalytics('UA-XXXXXXXX-X');
+
 	{{registration}}
 }
