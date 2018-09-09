d7e88a4f5f209418d06352062ff22ed229f5b877
Luke Edwards <luke.edwards05@gmail.com>
2018-09-09 13:40:29 -0700

    cli: handle empty values for `pretty` utils

--- packages/cli/lib/util/pretty.js
+++ packages/cli/lib/util/pretty.js
@@ -1,6 +1,7 @@
 const UNITS = ['B', 'kB', 'MB', 'GB'];
 
 exports.size = function (val) {
+	if (!val) return '0.00 kB';
 	let exp = Math.min(Math.floor(Math.log10(val) / 3), UNITS.length - 1) || 1;
 	let out = (val / Math.pow(1e3, exp)).toPrecision(3);
 	let idx = out.indexOf('.');
@@ -12,6 +13,6 @@ exports.size = function (val) {
 	return out + ' ' + UNITS[exp];
 }
 
-exports.time = function (ms) {
+exports.time = function (ms=0) {
 	return (ms / 1e3).toFixed(2) + 's';
 }
